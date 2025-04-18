cb26679dd4dbdff57af7325f2b2f4e165bfa2aeb
Matrixq35 <toomatris@gmail.com>
2025-04-18 13:27:44 +0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Задержка для имитации загрузки
-setTimeout(() => {
-  const loaderContainer = document.querySelector('.loader-container');
-  loaderContainer.style.display = 'none'; // Скрываем загрузочный экран
-}, 3000); // 3 секунды
